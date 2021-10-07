2469191dd8d1b2676e2b8ee09e6ca16a93d2f5a4
mgholamhossein <65259199+mgholamhossein@users.noreply.github.com>
2021-10-06 23:26:19 -0400

    updated
    removed unnecessary commented code

--- Config.py
+++ Config.py
@@ -31,38 +31,3 @@ LGB_PARAM = params = {
 
 }
 
-
-# {'target': 0.790220215581594, 'params': {'baggingFraction': 0.6120525191312296, 'baggingFreq': 6.0382220168365315,
-# 'baggingSeed': 24.115173633512136, 'featureFraction': 0.6499049925956872, 'learnRate': 0.014541399855351297,
-# 'maxDepth': 9.469356461614211, 'minChildSample': 24.935617888671082, 'minChildWeight': 12.735718561007117,
-# 'minDataInLeaf': 109.83378395711168, 'numLeaves': 56.19020295238769, 'scaleWeight': 3.604479561006932}}
-
-
-
-
-
-#
-# LGB_PARAM = params = {
-#     'n_jobs': 4,
-#     'n_estimators': 10000,
-#     # 'boosting_type': 'gbdt',
-#     # 'boost_from_average':'false',
-#     'learning_rate': 0.01,
-#     'colSam':0.575,
-#     'num_leaves': 20,
-#     'colSam':0.92,
-#     'scaleWeight':0.93,
-#     # 'num_threads':4,
-#     'max_depth': 3,
-#     'tree_learner': "serial",
-#     'feature_fraction': 0.6,
-#     'bagging_freq': 5,
-#     'bagging_fraction': 0.5,
-#     'min_data_in_leaf': 100,
-#     'min_child_samples': 62,
-#     'silent': -1,
-#     'verbose': -1,
-#     'max_bin': 255,
-#     # 'is_unbalance': 'true',
-#     'bagging_seed': 11
-# }
